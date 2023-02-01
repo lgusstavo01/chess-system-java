64456a830f1d44879e075607cdda084c8f256b87
Luiz Gustavo <l.gusstavo01@gmail.com>
2023-02-01 13:28:01 -0300

    Bispo

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
@@ -295,6 +296,8 @@ public class ChessMatch {
 		placeNewPiece('f', 2, new Pawn(board, Color.BRANCA));
 		placeNewPiece('g', 2, new Pawn(board, Color.BRANCA));
 		placeNewPiece('h', 2, new Pawn(board, Color.BRANCA));
+		placeNewPiece('c', 1, new Bishop(board, Color.BRANCA));
+		placeNewPiece('f', 1, new Bishop(board, Color.BRANCA));
 
 		placeNewPiece('a', 8, new Rook(board, Color.PRETA));
 		placeNewPiece('e', 8, new King(board, Color.PRETA));
@@ -307,6 +310,8 @@ public class ChessMatch {
 		placeNewPiece('f', 7, new Pawn(board, Color.PRETA));
 		placeNewPiece('g', 7, new Pawn(board, Color.PRETA));
 		placeNewPiece('h', 7, new Pawn(board, Color.PRETA));
+		placeNewPiece('c', 8, new Bishop(board, Color.PRETA));
+		placeNewPiece('f', 8, new Bishop(board, Color.PRETA));
 
 	}
 }
